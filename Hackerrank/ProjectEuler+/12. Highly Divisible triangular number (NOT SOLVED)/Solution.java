23308d45b61785362e46d541802cdb89fa1ea56b
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-01-02 16:24:23 +0545

    Project Euler #12 solved

--- Hackerrank/ProjectEuler+/12. Highly Divisible triangular number (NOT SOLVED)/Solution.java
+++ Hackerrank/ProjectEuler+/12. Highly Divisible triangular number (NOT SOLVED)/Solution.java
@@ -1,31 +1,38 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Solution{
 
-	static int numberOfFactors(int n){
-		int count = 0;
-		for(int i=1; i*i<n; i++){
-			if(n%i==0) count+=2;
-		}
-		if(Math.sqrt(n) == (int)Math.sqrt(n)) count++;
-		return count;
-	}
+    static HashMap numFactors = new HashMap<Integer, Integer>();
 
-	static int firstTriangleNumber(int n){
-		int tNumber;
-		for(int i = 1; true; i++){
-			tNumber = (i*(i+1))/2;
-			if(numberOfFactors(tNumber) > n) return tNumber;
-		}
-	}
+    static int numberOfFactors(int n){
+        if (numFactors.containsKey(n))
+            return ((Integer)(numFactors.get(n))).intValue();
+        
+        int count = 0;
+        for(int i=1; i*i<n; i++){
+            if(n%i==0) count+=2;
+        }
+        if(Math.sqrt(n) == (int)Math.sqrt(n)) count++;
+        numFactors.put(n, count);
+        return count;
+    }
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int T = in.nextInt();
-		for(int i = 0; i< T; i++){
-			int number = in.nextInt();
-			System.out.println(firstTriangleNumber(number));
-		}		
-	}
+    static int firstTriangleNumber(int n){
+        int tNumber;
+        for(int i = 1; true; i++){
+            tNumber = (i*(i+1))/2;
+            if(numberOfFactors(tNumber) > n) return tNumber;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        for(int i = 0; i< T; i++){
+            int number = in.nextInt();
+            System.out.println(firstTriangleNumber(number));
+        }        
+    }
 
 }
--- Hackerrank/ProjectEuler+/17. Number to words/solution.py
+++ Hackerrank/ProjectEuler+/17. Number to words/solution.py
@@ -1,11 +1,20 @@
-tail = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen', 'Twenty', 'Twenty One', 'Twenty Two', 'Twenty Three', 'Twenty Four', 'Twenty Five', 'Twenty Six', 'Twenty Seven', 'Twenty Eight', 'Twenty Nine', 'Thirty', 'Thirty One', 'Thirty Two', 'Thirty Three', 'Thirty Four', 'Thirty Five', 'Thirty Six', 'Thirty Seven', 'Thirty Eight', 'Thirty Nine', 'Forty', 'Forty One', 'Forty Two', 'Forty Three', 'Forty Four', 'Forty Five', 'Forty Six', 'Forty Seven', 'Forty Eight', 'Forty Nine', 'Fifty', 'Fifty One', 'Fifty Two', 'Fifty Three', 'Fifty Four', 'Fifty Five', 'Fifty Six', 'Fifty Seven', 'Fifty Eight', 'Fifty Nine', 'Sixty', 'Sixty One', 'Sixty Two', 'Sixty Three', 'Sixty Four', 'Sixty Five', 'Sixty Six', 'Sixty Seven', 'Sixty Eight', 'Sixty Nine', 'Seventy', 'Seventy One', 'Seventy Two', 'Seventy Three', 'Seventy Four', 'Seventy Five', 'Seventy Six', 'Seventy Seven', 'Seventy Eight', 'Seventy Nine', 'Eighty', 'Eighty One', 'Eighty Two', 'Eighty Three', 'Eighty Four', 'Eighty Five', 'Eighty Six', 'Eighty Seven', 'Eighty Eight', 'Eighty Nine', 'Ninety', 'Ninety One', 'Ninety Two', 'Ninety Three', 'Ninety Four', 'Ninety Five', 'Ninety Six', 'Ninety Seven', 'Ninety Eight', 'Ninety Nine']
-places = ['', '', ' Thousand', ' Million', ' Billion', ' Trillion']
+ones = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+tens = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+places = ['', '', ' Thousand', ' Million', ' Billion', ' Trillion', ' Quadrillion', ' Quintillion', ' Sextillion', ' Septillion', ' Octillion', ' Nonillion', ' Decillion', ' Undecillion', ' Duodecillion', ' Tredecillion', ' Quattuordecillion', ' Quindecillion', ' Sexdecillion', ' Septendecillion', ' Octodecillion', ' Novemdecillion', ' Vigintillion']
+
+def tail(n):
+	if n < 20:
+		return ones[n]
+	else:
+		t = n // 10
+		o = n % 10
+		return tens[t] + ('' if t==0 else ' ') + ones[o] 
 
 
 def printHundred(num):
 	h = num // 100
 	to = num % 100
-	return tail[h] + ('' if h==0 else ' Hundred') + ('' if to==0 or h==0 else ' ') + tail[to]
+	return tail(h) + ('' if h==0 else ' Hundred') + ('' if to==0 or h==0 else ' ') + tail(to)
 
 t = int(input())
 
